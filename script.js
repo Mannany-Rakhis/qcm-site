512dc8bcef3162fe65bf9449a5f1d777d73bf15e
Izayoii <rakhisahmat3600@gmail.com>
2026-04-03 12:25:08 +0200

    feat qcm script

--- script.js
+++ script.js
@@ -1,12 +1,66 @@
+function shuffle(array){
+
+for(let i=array.length-1;i>0;i--){
+
+let j=Math.floor(Math.random()*(i+1));
+
+[array[i],array[j]]=[array[j],array[i]];
+
+}
+
+}
+
+shuffle(banque);
+
+let questions=banque.slice(0,30);
+
+let quiz=document.getElementById("quiz");
+
 let answeredSet=new Set();
+
+questions.forEach((q,i)=>{
+
+let div=document.createElement("div");
+
+div.className="question";
+
+let html="<p>"+(i+1)+". "+q.q+"</p>";
+
+let reps=[...q.a];
+
+shuffle(reps);
+
+reps.forEach(rep=>{
+
+html+=`
+
+<label>
+
+<input type="radio"
+name="q${i}"
+value="${q.a.indexOf(rep)}">
+
+${rep}
+
+</label>
+
+`;
+
+});
+
+html+=`<div class="exp" id="exp${i}"></div>`;
+
+div.innerHTML=html;
+
+quiz.appendChild(div);
+
+});
 
 document.addEventListener("change",(e)=>{
 
 if(e.target.type=="radio"){
 
-let name=e.target.name;
-
-answeredSet.add(name);
+answeredSet.add(e.target.name);
 
 let progress=
 (answeredSet.size/30)*100;
@@ -17,3 +71,54 @@ progress+"%";
 }
 
 });
+
+function corriger(){
+
+let score=0;
+
+let divs=document.querySelectorAll(".question");
+
+questions.forEach((q,i)=>{
+
+let choix=document.querySelector(
+`input[name=q${i}]:checked`
+);
+
+if(!choix)return;
+
+if(choix.value==q.r){
+
+score++;
+
+divs[i].classList.add("correct");
+
+}
+
+else{
+
+divs[i].classList.add("wrong");
+
+}
+
+document.getElementById("exp"+i).innerHTML=
+q.exp;
+
+});
+
+document.getElementById("result").innerHTML=
+
+"Score : "+score+"/30";
+
+let best=localStorage.getItem("best");
+
+if(!best || score>best){
+
+localStorage.setItem("best",score);
+
+}
+
+document.getElementById("best").innerHTML=
+
+"Meilleur score : "+localStorage.getItem("best");
+
+}
