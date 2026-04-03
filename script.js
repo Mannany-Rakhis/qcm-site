ad91a1a4d9b550614b390f0734f87e502f66cca1
Izayoii <rakhisahmat3600@gmail.com>
2026-04-03 11:49:00 +0200

    feat qcm script

--- script.js
+++ script.js
@@ -1,250 +1,10 @@
-let banque=[
-
-{
-q:"Pourquoi documenter ?",
-a:["Décorer","Comprendre code","Ralentir","Supprimer tests"],
-r:1
-},
-
-{
-q:"Une API est :",
-a:[
-"communication apps",
-"base données",
-"framework",
-"langage"
-],
-r:0
-},
-
-{
-q:"GET sert à :",
-a:["Créer","Lire","Modifier","Delete"],
-r:1
-},
-
-{
-q:"POST sert à :",
-a:["Lire","Créer","Delete","Modifier"],
-r:1
-},
-
-{
-q:"PUT fait :",
-a:[
-"modifier partiel",
-"remplacer",
-"lire",
-"delete"
-],
-r:1
-},
-
-{
-q:"PATCH fait :",
-a:[
-"modifier partiel",
-"lire",
-"créer",
-"delete"
-],
-r:0
-},
-
-{
-q:"404 signifie :",
-a:[
-"OK",
-"pas trouvé",
-"server down",
-"redirect"
-],
-r:1
-},
-
-{
-q:"200 signifie :",
-a:[
-"OK",
-"fail",
-"delete",
-"error"
-],
-r:0
-},
-
-{
-q:"MkDocs est :",
-a:[
-"éditeur",
-"générateur doc",
-"database",
-"api"
-],
-r:1
-},
-
-{
-q:"Commande serveur MkDocs :",
-a:[
-"serve",
-"run",
-"start",
-"launch"
-],
-r:0
-},
-
-{
-q:"Commande build :",
-a:[
-"build",
-"compile",
-"make",
-"generate"
-],
-r:0
-},
-
-{
-q:"Markdown gras :",
-a:[
-"*text*",
-"**text**",
-"text",
-"//"
-],
-r:1
-},
-
-{
-q:"Inline code :",
-a:[
-"```code```",
-"`code`",
-"code",
-"//"
-],
-r:1
-},
-
-{
-q:"Bloc code utilise :",
-a:[
-"1 backtick",
-"2",
-"3",
-"4"
-],
-r:2
-},
-
-{
-q:"Lien markdown :",
-a:[
-"[url](text)",
-"[text](url)",
-"url()",
-"<url>"
-],
-r:1
-},
-
-{
-q:"Liste tâche :",
-a:[
-"(x)",
-"- [x]",
-"[x]",
-"{x}"
-],
-r:1
-},
-
-{
-q:"Notion fonctionne avec :",
-a:[
-"pages",
-"files",
-"servers",
-"api"
-],
-r:0
-},
-
-{
-q:"/code crée :",
-a:[
-"titre",
-"code",
-"image",
-"table"
-],
-r:1
-},
-
-{
-q:"Kanban :",
-a:[
-"board",
-"table",
-"graph",
-"text"
-],
-r:0
-},
-
-{
-q:"Excel somme :",
-a:[
-"ADD",
-"SUM",
-"SOMME",
-"TOTAL"
-],
-r:2
-},
-
-{
-q:"Powerpoint règle :",
-a:[
-"5x5",
-"6x6",
-"10x10",
-"4x4"
-],
-r:1
-},
-
-];
-
-// duplication pour 60
-
-while(banque.length<60){
-
-banque.push(
-banque[Math.floor(Math.random()*21)]
-);
-
-}
-
-function shuffle(array){
-
-for(let i=array.length-1;i>0;i--){
-
-let j=Math.floor(Math.random()*(i+1));
-
-[array[i],array[j]]=[array[j],array[i]];
-
-}
-
-}
-
 shuffle(banque);
 
 let questions=banque.slice(0,30);
 
 let quiz=document.getElementById("quiz");
+
+let answered=0;
 
 questions.forEach((q,i)=>{
 
@@ -261,18 +21,24 @@ shuffle(reps);
 reps.forEach(rep=>{
 
 html+=`
+
 <label>
 
 <input type="radio"
+
 name="q${i}"
+
 value="${q.a.indexOf(rep)}">
 
 ${rep}
 
-</label><br>
+</label>
+
 `;
 
 });
+
+html+=`<div class="exp" id="exp${i}"></div>`;
 
 div.innerHTML=html;
 
@@ -280,31 +46,27 @@ quiz.appendChild(div);
 
 });
 
-let answered=0;
+function shuffle(array){
 
-document.addEventListener("change",()=>{
+for(let i=array.length-1;i>0;i--){
 
-answered++;
+let j=Math.floor(Math.random()*(i+1));
 
-let progress=
-(answered/30)*100;
+[array[i],array[j]]=[array[j],array[i]];
 
-document.getElementById("bar").style.width=
-progress+"%";
+}
 
-});
+}
 
 function corriger(){
 
 let score=0;
 
-let divs=
-document.querySelectorAll(".question");
+let divs=document.querySelectorAll(".question");
 
 questions.forEach((q,i)=>{
 
-let choix=
-document.querySelector(
+let choix=document.querySelector(
 `input[name=q${i}]:checked`
 );
 
@@ -324,35 +86,26 @@ divs[i].classList.add("wrong");
 
 }
 
+document.getElementById("exp"+i).innerHTML=
+
+q.exp;
+
 });
 
 document.getElementById("result").innerHTML=
 
 "Score : "+score+"/30";
 
-}
+let best=localStorage.getItem("best");
 
-let time=1200;
+if(!best || score>best){
 
-let timer=setInterval(()=>{
-
-time--;
-
-let min=Math.floor(time/60);
-
-let sec=time%60;
-
-document.getElementById("timer").innerHTML=
-
-min+":"+
-(sec<10?"0":"")+sec;
-
-if(time==0){
-
-corriger();
-
-clearInterval(timer);
+localStorage.setItem("best",score);
 
 }
 
-},1000);
+document.getElementById("best").innerHTML=
+
+"Meilleur score : "+localStorage.getItem("best");
+
+}
